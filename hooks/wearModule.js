7064c567edc78c5fc5dabc1f2dd4ebaf9073aaf3
pcamilojunior <paulo.camilo.junior@outsystems.com>
2025-04-11 19:05:39 +0200

    fix issue settings gradle

--- hooks/wearModule.js
+++ hooks/wearModule.js
@@ -3,22 +3,13 @@ const path = require('path');
 
 module.exports = function (context) {
   const platformRoot = path.join(context.opts.projectRoot, 'platforms', 'android');
-  const settingsGradle = path.join(platformRoot, 'settings.gradle');
-  const smartwatchInclude = "\ninclude ':smartwatch'\nproject(':smartwatch').projectDir = new File(rootProject.projectDir, 'smartwatch')\n";
+  const appGradle = path.join(platformRoot, 'app', 'build.gradle');
 
-  // Atualizar settings.gradle
-  if (fs.existsSync(settingsGradle)) {
-    let content = fs.readFileSync(settingsGradle, 'utf-8');
-    if (!content.includes("include ':smartwatch'")) {
-      fs.appendFileSync(settingsGradle, smartwatchInclude);
-      console.log('✅ settings.gradle atualizado com módulo :smartwatch');
-    } else {
-      console.log('ℹ️ settings.gradle já contém o módulo :smartwatch');
-    }
-  }
+  const targetLine = `implementation fileTree(dir: 'libs', include: '*.jar')`;
+  const insertLine = `wearApp project(':smartwatch')`;
 
   // Atualizar build.gradle (app)
-  /**if (fs.existsSync(appGradle)) {
+  if (fs.existsSync(appGradle)) {
     let content = fs.readFileSync(appGradle, 'utf-8');
 
     if (!content.includes(insertLine)) {
@@ -30,5 +21,5 @@ module.exports = function (context) {
     }
   } else {
     console.warn('⚠️ app/build.gradle não encontrado');
-  }**/
+  }
 };
